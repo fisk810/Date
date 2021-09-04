a4ddd12bc7700e0bc1204e64d38ba1c8aa648eae
fisk810 <marc571m@edu.easj.dk>
2021-09-04 13:58:50 +0200

    done

---
+++ src/com/company/Main.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Main {
+
+
+
+    public static void main(String[] args) {
+        String day = "Monday";
+        int date = 4;
+        String month = "September";
+        int year = 2021;
+	// write your code here
+        System.out.println("Hello world!");
+        System.out.println("American format: " + day + ", " + month + " " + date + ", " + year);
+        System.out.println("European format: " + day + " " + date + " " + month + " " + year);
+    }
+}
